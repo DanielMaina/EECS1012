360b2d602c434c389ec5e114af13bacfcbd4a030
Dan <danielmaina6@gmail.com>
2023-04-08 14:01:19 -0400

    styling almost done

--- js/crypto.js
+++ js/crypto.js
@@ -1,4 +1,7 @@
+var ltc = document.querySelector(".priceD")
 var btc = document.querySelector(".priceC")
+var eth = document.querySelector(".priceB")
+var doge = document.querySelector(".priceA")
 
 var liveprice = {
     "async": true,
@@ -11,6 +14,10 @@ var liveprice = {
 
 $.ajax(liveprice).done(function (response){
     btc.innerHTML = response.bitcoin.usd;
+    ltc.innerHTML = response.litecoin.usd;
+    eth.innerHTML = response.ethereum.usd;
+    doge.innerHTML = response.dogecoin.usd;
+
 });
 
 document.querySelector(".crypto").classList.remove("loading");
